43a0dbd6a303734d8bad84a6705e6f2c15081ac1
DomCamillo <dominicmoerth@gmail.com>
2025-12-19 14:56:34 +0100

    video model

--- video/models.py
+++ video/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    video_file = models.FileField(upload_to='videos/')
+
+    def __str__(self):
+        return self.title

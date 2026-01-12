a50d156b3f4d9433223cc80913fc53f9d10ea1cd
DomCamillo <dominicmoerth@gmail.com>
2026-01-12 18:08:20 +0100

    added proccess video function and improved signal

--- video_content/signals.py
+++ video_content/signals.py
@@ -6,12 +6,11 @@ import django_rq
 
 @receiver(post_save, sender=Video)
 def video_post_safe(sender, instance, created, **kwargs):
-    """everytime a video is saved, this signal is triggerd """
+    """everytime a video is saved this signal is triggerd """
     if created and instance.video_file:
         print(f"Video {instance.title} has been uploaded")
-        from video_content.tasks import convert_1080p, convert_720p, convert_480p, generate_thunbnail, convert_hls
+        from video_content.tasks import process_video
         queue = django_rq.get_queue('default')
-        queue.enqueue(convert_480p, instance.id)
-        queue.enqueue(convert_720p, instance.id)
-        queue.enqueue(convert_1080p, instance.id)
-        print(f"Video{instance.id} qued for processing")
+        queue.enqueue(process_video, instance.id)
+        print(f"video {instance.id} {instance.title} has been queued for processing")
+
--- video_content/tasks.py
+++ video_content/tasks.py
@@ -3,6 +3,13 @@ import os
 from django.conf import settings
 from .models import Video
 
+
+"""Dein Server speichert Videos als Dateien.
+Die DB sagt, wo sie liegen.
+Der Browser lädt URLs.
+HLS setzt die Dateien wieder zu einem Video zusammen."""
+
+
 def convert_480p(source):
     target = source + '480p.mp4'
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source,target)
@@ -27,11 +34,72 @@ def generate_thunbnail(source, time_frame=1):
 
 #ffmpeg -i input.mp4 -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls output.m3u8 convert to hls format
 # ffmpeg -i input_video.mp4 -c copy -hls_time 5 - hls_list_size 0 -f hls output.m3u8
-def convert_hls(source):
-    target = source + '.m3u8'
-    cmd = 'ffmpeg -i "{}" -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls "{}"'.format(source,target)
-    subprocess.run(cmd)
+def convert_hls(video_id, source_path):
+    """converte video to hls format in 3 different resolutions
+    creates folders and files in media/videos/hls/<video_id>/<resolution>/
+    and runs ffmpeg command to convert the video"""
+    resolutions = {
+        '480p': '854:480',
+        '720p': '1280:720',
+        '1080p': '1920:1080'
+    }
+    for res_name, scale in resolutions.itmes():
+        print(f"converting to {res_name}")
+        output_dir = os.path.join(
+            settings.MEDIA_ROOT,
+            'videos',
+            'hls',
+            str(video_id),
+            res_name
+        )
+        os.makedirs(output_dir, exist_ok=True)
+
+        playlist_path = os.path.join(output_dir,'index.m8u3')
+        segment_pattern = os.path.join(output_dir, 'segment%03d.ts')
+
+        cmd = [
+            'ffmpeg',
+            '-i', source_path,
+            '-vf', f'scale={scale}',
+            '-c:v', 'libx264',
+            '-crf', '23',
+            '-c:a', 'aac',
+            '-hls_time', '10',
+            '-hls_list_size', '0',
+            '-hls_segment_filename', segment_pattern,
+            '-f', 'hls',
+            playlist_path
+        ]
+        try:
+            result = subprocess.run(cmd, check=True,  capture_output=True, text=True)
+            print(f"{res_name} Done")
+        except subprocess.CalledProcessError as error:
+            print(f"Error converting to {res_name}: {error.stderr}")
+
+
+
+
 
 
 def process_video(video_id):
-    pass
+    """gets video by id and process it into different resolutions"""
+    print(f"Proccesing video id: {video_id}")
+    try:
+        video = Video.objects.get(id=video_id)
+        video.status = 'processing'
+        video.save()
+
+        source_path = video.video_file.path
+        print(f"Source path: {source_path}")
+
+        convert_hls(source_path)
+        video.status = 'completed'
+        video.hls_480p_path =f'videos/hls/{video.id}/480p'
+        video.hls_720p_path = f'videos/hls/{video.id}/720p'
+        video.hls_1080p_path = f'videos/hls/{video.id}/1080p'
+        video.save()
+
+        print(f"video id: {video_id} proccessed successfully")
+    except Exception as error:
+        print(f"{error}")
+        video.status = 'failed'
